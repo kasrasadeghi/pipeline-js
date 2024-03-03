f4f24d1ed7111e24e4ab30c0c353266f772f9645
kasrasadeghi <kasra.sadeghi1@gmail.com>
2024-03-02 20:58:41 -0800

    sketch out api support in the simple server

--- simple_server.py
+++ simple_server.py
@@ -1,3 +1,9 @@
+# current TODO for compatibility with pipeline python impl
+# GET /api/get/<note> - returns raw text of note
+# GET /api/list/<repo> - returns a json of all note uuids
+
+# PUT /api/put/<note> - stores the body into the note file
+
 # Python3.7+
 import socket
 import os
@@ -11,15 +17,19 @@ listen_socket.listen(1)
 print(f'Serving HTTP on port {PORT} ...')
 while True:
     client_connection, client_address = listen_socket.accept()
-    request_data = client_connection.recv(1024)
+    request_data = client_connection.recv(1024)  # TODO receive more?
     first_line, rest = request_data.decode('utf-8').split('\n', 1)
 
     print(first_line)
     parts = first_line.split()
-    if len(parts) == 2:
+
+    # this almost never happens
+    if len(parts) == 2: # GET /disc/bigmac-js/24b1bb0d-3148-4d3d-addb-3b44e4259a8e
         method, path = parts
         httpver = "HTTP/1.1"
-    elif len(parts) == 3:
+
+    # usually this one happens
+    elif len(parts) == 3: # GET /disc/bigmac-js/24b1bb0d-3148-4d3d-addb-3b44e4259a8e HTTP/1.1
         method, path, httpver = parts
     else:
         print('huh')
@@ -35,10 +45,44 @@ Hello, World!
         client_connection.close()
         continue
 
-    path = path.removeprefix("/")
-    if path.startswith('disc') or path.startswith('edit') or path.startswith("list") or path == '':
+    # Handle MPA paths
+
+    if path.startswith('/disc') or path.startswith('edit') or path.startswith("/list") or path == '/' or path == '':
         path = 'index.html'
+
+    # Handle API paths
+
+    if path.startswith('/api'):
+        path = path.removeprefix('/api')
+        http_response = b"""\
+HTTP/1.1 200 OK
+
+The Pipeline API listing:
+GET /api/list/<repo>
+GET /api/get/<note>
+PUT /api/put/<note>
+"""
+
+        if path.startswith('/list/') and method == 'GET':
+            repo = path.removeprefix('/list/')
+            print(repo)
+            print(rest)
+        elif path.startswith('/get/') and method == 'GET':
+            note = path.removeprefix('/get/')
+            print(note)
+            print(rest)
+        elif path.startswith('/put/') and method == 'PUT':
+            note = path.removeprefix('/put/')
+            print(note)
+            print(rest)
+        
+        client_connection.sendall(http_response)
+        client_connection.close()
+        continue
+
+    # Handle Static paths
     
+    path = path.removeprefix('/')
     if not os.path.exists(path):
         http_response = b"""\
 HTTP/1.1 400 NOT_FOUND 
