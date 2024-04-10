bfe5b1dd519dc68f4db19a41a0b2066357f0b43b
kasrasadeghi <kasra.sadeghi1@gmail.com>
2024-04-11 07:41:00 +0900

    fix navigator typo

--- indexed-fs.js
+++ indexed-fs.js
@@ -1555,7 +1555,7 @@ async function perf(func) {
 }
 
 function registerServiceWorker() {
-  if ('serviceWorker' in nagivigator) {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('/service-worker.js').then(
         (registration) => { console.log("ServiceWorker registration successful with scope: ", registration.scope); },
