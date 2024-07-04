9e744e140483b27ac390be46e44314c689380af1
kasrasadeghi <kasra.sadeghi1@gmail.com>
2024-07-04 17:13:07 -0400

    DONE failed test for double journal

--- docker/test.py
+++ docker/test.py
@@ -35,20 +35,6 @@ if args.no_docker:
 else:
     chrome_options.add_argument("--ssl-cert-path=/opt/selenium/cert/cert.pem")
 
-print("Setting up WebDriver...")
-
-# Set up Selenium WebDriver
-if args.no_docker:
-    driver = webdriver.Chrome(options=chrome_options)
-else:
-    driver = webdriver.Remote(
-        command_executor='http://selenium:4444/wd/hub',
-        options=chrome_options
-    )
-print("WebDriver set up successfully.")
-print(f"Chrome version: {driver.capabilities['browserVersion']}")
-print(f"ChromeDriver version: {driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]}")
-
 # --- UTILS ------------------------------------------------------------------
 
 def get_pipeline_url():
@@ -57,28 +43,54 @@ def get_pipeline_url():
     else:
         return "https://server:5000"
 
-def browser_wrapper(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except TimeoutException:
-            print("ERROR: Timeout waiting for page to load")
-        except WebDriverException as e:
-            print(f"WebDriver exception: {str(e)}")
-        except Exception as e:
-            print(f"Test failed: {str(e)}")
-        finally:
-            # Close the browser
-            print("Closing browser...")
-            driver.quit()
-    return wrapper
+def create_driver():
+    print("Setting up WebDriver...")
+
+    # Set up Selenium WebDriver
+    if args.no_docker:
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+        driver = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            options=chrome_options
+        )
+    print("WebDriver set up successfully.")
+    print(f"Chrome version: {driver.capabilities['browserVersion']}")
+    print(f"ChromeDriver version: {driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]}")
+    return driver
+
+def browser_wrapper(close = True):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except TimeoutException:
+                print("ERROR: Timeout waiting for page to load")
+            except WebDriverException as e:
+                print(f"WebDriver exception: {str(e)}")
+            except AssertionError as e:
+                print(f"Test failed: {str(e)}")
+            except Exception as e:
+                print(f"Test failed: {str(e)}")
+        return wrapper
+    return decorator
 
 ENTER_KEY = u'\ue007'
 
+def el_id(driver, id):
+    try:
+        result = driver.find_element(By.ID, id)
+        return result
+    except NoSuchElementException as e:
+        print(f"Element with ID {id} not found")
+        result = None
+        breakpoint()
+        return result
+
 # --- TESTS ------------------------------------------------------------------
 
-@browser_wrapper
-def test_first_time_setup():
+@browser_wrapper(close=False)
+def test_first_time_setup(driver):
 
     print("Navigating to Flask app...")
     driver.get(get_pipeline_url())
@@ -94,7 +106,7 @@ def test_first_time_setup():
     # driver.execute_cdp_cmd("Debugger.enable", {})
     # driver.execute_cdp_cmd("Debugger.setPauseOnExceptions", {"state": "all"})
 
-    time.sleep(2)
+    time.sleep(1)
 
     print('Test creating a new repo, sending a message, syncing the note')
 
@@ -103,38 +115,28 @@ def test_first_time_setup():
     if os.path.exists('notes/' + repo_name):
         shutil.rmtree('notes/' + repo_name)
 
-    def el_id(id):
-        try:
-            result = driver.find_element(By.ID, id)
-            return result
-        except NoSuchElementException as e:
-            print(f"Element with ID {id} not found")
-            result = None
-            breakpoint()
-            return result
+    print('type in "selenium_test"')
+    el_id(driver, "local_repo_name").send_keys("selenium_test")
+    el_id(driver, "local_repo_name_button").click()
 
-    # type in "selenium_test"
-    el_id("local_repo_name").send_keys("selenium_test")
-    el_id("local_repo_name_button").click()
-
-    # click journal button
-    el_id("journal_button").click()
+    print("click journal button")
+    el_id(driver, "journal_button").click()
 
     # check that page loads
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.TAG_NAME, "body"))
     )
 
-    time.sleep(2)
+    time.sleep(1)
 
     # click message input box
-    el_id("msg_input").send_keys("selenium test message")
-    el_id("msg_input").send_keys(ENTER_KEY)
+    el_id(driver, "msg_input").send_keys("selenium test message")
+    el_id(driver, "msg_input").send_keys(ENTER_KEY)
 
     time.sleep(1)
 
     # press enter on empty to sync
-    el_id("msg_input").send_keys(ENTER_KEY)
+    el_id(driver, "msg_input").send_keys(ENTER_KEY)
 
     # get current uuid using javascript
     current_uuid = driver.execute_script("return getCurrentNoteUuid()")
@@ -142,41 +144,97 @@ def test_first_time_setup():
     assert os.path.exists('notes/' + repo_name)
     assert os.path.exists('notes/' + current_uuid)
 
-    time.sleep(2)
+    time.sleep(1)
 
     print('Test successful')
     print('Test creating a new message on a new day')
     driver.execute_script("global.mock_now = new Date(); global.mock_now.setDate(global.mock_now.getDate() + 1);")
 
-    el_id("msg_input").send_keys("selenium testing on a new day")
-    el_id("msg_input").send_keys(ENTER_KEY)
+    el_id(driver, "msg_input").send_keys("selenium testing on a new day")
+    el_id(driver, "msg_input").send_keys(ENTER_KEY)
 
     # Keep the browser open for a while to allow viewing
-    time.sleep(30)
+    time.sleep(2)
 
-@browser_wrapper
-def test_new_day_double_journal():
-    # BUG tabs don't synchronize journal creation with other tabs on the same machine
-    # INVARIANT each machine has a unique journal for each day
-    # REPRODUCE
-    # - open 2 tabs (tab A and tab B) on day X
-    # - both of them should be on the same journal entry, assuming they are opened one after another
-    # - BUT THEN
-    # - increment the day for both tabs
-    # - go to journal in tab A
-    # - go to journal in tab B
-    # - RESULT they will be different journal notes.  (see the uuid)
-    # - HYPOTHESIS this is because the notes are created in the cache and written back to IDB, 
-    #     but the other tab doesn't rebuild/ check its cache before making a new note.
 
-    # open another tab and set the day one more day in the future
-    driver.execute_script("window.open('https://server:5000');")
+# BUG tabs don't synchronize journal creation with other tabs on the same machine
+# INVARIANT each machine has a unique journal for each day
+# REPRODUCE
+# - open 2 tabs (tab A and tab B) on day X
+# - both of them should be on the same journal entry, assuming they are opened one after another
+# - BUT THEN
+# - increment the day for both tabs
+# - go to journal in tab A
+# - go to journal in tab B
+# - RESULT they will be different journal notes.  (see the uuid)
+# - HYPOTHESIS this is because the notes are created in the cache and written back to IDB, 
+#     but the other tab doesn't rebuild/ check its cache before making a new note.
+@browser_wrapper(close=True)
+def test_new_day_double_journal(driver):
+
+    # there should already be one tab open on journal X+1 from first time setup
+
+    # open a new tab
+    driver.execute_script(f"window.open('{get_pipeline_url()}');")
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "body"))
+    )
+
+    # tab A to X+2
+    driver.switch_to.window(driver.window_handles[0])
+    driver.execute_script("global.mock_now = new Date(); global.mock_now.setDate(global.mock_now.getDate() + 2);")
+    driver.execute_script("document.getElementById('journal_button').click();")
+
+    time.sleep(2)
+
+    # tab B to X+2
     driver.switch_to.window(driver.window_handles[1])
     driver.execute_script("global.mock_now = new Date(); global.mock_now.setDate(global.mock_now.getDate() + 2);")
+    driver.execute_script("document.getElementById('journal_button').click();")
+    time.sleep(1)
+
+    # the two uuids shouldn't be different, but they are
+    driver.switch_to.window(driver.window_handles[0])
+    tab_a_uuid = driver.execute_script("return getCurrentNoteUuid()")
+    driver.switch_to.window(driver.window_handles[1])
+    tab_b_uuid = driver.execute_script("return getCurrentNoteUuid()")
+
+    print(tab_a_uuid, tab_b_uuid)
+    # assert tab_a_url == tab_b_url
+
+    # press enter to upload
+    driver.switch_to.window(driver.window_handles[0])
+    el_id(driver, "msg_input").send_keys("tab A, day X+2")
+    el_id(driver, "msg_input").send_keys(ENTER_KEY)
+    time.sleep(1)
+
+    driver.switch_to.window(driver.window_handles[1])
+    el_id(driver, "msg_input").send_keys("tab B, day X+2")
+    el_id(driver, "msg_input").send_keys(ENTER_KEY)
+
+    # after they sync, they will converge to the same uuid, but there will be an extra one made for day X+2
+    # they'll go to the one that is lexicographically first (3... < a... with hex ordering)
+
+    # we can check the files and see that there are two that have "Title: July 6th, 2024" in them
+    for l in os.listdir('notes/selenium_test'):
+        count = 0
+        with open(f'notes/selenium_test/{l}') as f:
+            if "Title: July 6th, 2024" in f.read():
+                print(l)
+                count += 1
+        assert count == 1
+
+    time.sleep(10)
 
 
 def main():
-    test_first_time_setup()
-    input()
-    test_new_day_double_journal()
+    driver = create_driver()
+    test_first_time_setup(driver)
+    test_new_day_double_journal(driver)
+    driver.quit()
+    print("Test script completed.")
+
+if __name__ == "__main__":
+    main()
     print("Test script completed.")
