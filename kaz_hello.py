549671864031a29628224e31a61ac8b20946bdca
kasrasadeghi <kasra.sadeghi1@gmail.com>
2024-07-08 20:19:23 -0400

    use `run` in hello world

--- kaz_hello.py
+++ kaz_hello.py
@@ -1,28 +1,10 @@
-from kazhttp import HTTP_OK, HTTP_NOT_FOUND, HTTP_OK_JSON, allow_cors_for_localhost, receive_headers_and_content, create_server_socket
+from kazhttp import HTTP_OK, run
 
 import sys
-import traceback
-from datetime import datetime
+
+def handle_request(request):
+    return HTTP_OK(b'Hello World!', mimetype=b"text/plain")
 
 HOST, PORT = '', int(sys.argv[1])
-listen_socket, https = create_server_socket(HOST, PORT)
-while True:
-    try:
-        print('----------------------------------------')
-        client_connection, client_address = listen_socket.accept()
-        print(datetime.now(), client_address) # (address: string, port: int)
-        request = receive_headers_and_content(client_connection)
-        if request is None:
-            continue
+run(host=HOST, port=PORT, handle_request=handle_request)
 
-        method = request['method']
-        path = request['path']
-        headers = request['headers']
-        body = request['body']
-
-        http_response = HTTP_OK(b'Hello World!', mimetype=b"text/plain")
-        print("RESPONSE:", http_response)
-        client_connection.sendall(http_response)
-        client_connection.close()
-    except Exception as e:
-        traceback.print_exc()
